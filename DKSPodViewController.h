3bdef41ea4c574fb16c8dccdff324aab8d0138c0
duks <duks@robooot.com>
2016-08-22 21:53:22 +0800

    first commit

---
+++ DKSPodViewController.h
@@ -0,0 +1,18 @@
+//
+//  DKSPodViewController.h
+//  PZHAutoLayoutDemo
+//
+//  Created by aDu on 16/8/22.
+//  Copyright © 2016年 nuomi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DKSPodViewController : UIViewController
+
+/**
+ *  第一个Pod工程
+ */
+- (void)firstPod;
+
+@end
---
+++ DKSPodViewController.m
@@ -0,0 +1,26 @@
+//
+//  DKSPodViewController.m
+//  PZHAutoLayoutDemo
+//
+//  Created by aDu on 16/8/22.
+//  Copyright © 2016年 nuomi. All rights reserved.
+//
+
+#import "DKSPodViewController.h"
+
+@interface DKSPodViewController ()
+
+@end
+
+@implementation DKSPodViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)firstPod
+{
+    NSLog(@"第一个Pod工程");
+}
+
+@end
